a9b5885dd864497942c8184d70b1e9aded2b5849
Paul Calnan <pwc3@users.noreply.github.com>
2020-05-07 13:08:05 -0400

    Initial commit, extracted from other project

---
+++ README.md
@@ -0,0 +1,3 @@
+# ExceptionHandler
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import ExceptionHandlerTests
+
+var tests = [XCTestCaseEntry]()
+tests += ExceptionHandlerTests.allTests()
+XCTMain(tests)
